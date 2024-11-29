ce05bf525ae52eb99c8d3b0dc7b5f328957cbd91
GG-Pharaoh <oxdiablo20@gmail.com>
2024-11-29 05:47:18 -0800

    frontend and backend communicating seamlessly now

--- cashpoint-pos-backend/server.js
+++ cashpoint-pos-backend/server.js
@@ -1,34 +1,27 @@
+// File: backend/server.js
+import express from 'express';
+import cors from 'cors'; // Import CORS
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import saleRoutes from './routes/saleRoutes.js';
 
-// Import necessary modules
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import userRoutes from "./routes/userRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import saleRoutes from "./routes/saleRoutes.js";
-
-
-// Load environment variables
 dotenv.config();
 
-// Create Express app
 const app = express();
 
-// Middleware to parse JSON
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
-
-// Routes for different API endpoints 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", saleRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("Cashpoint POS Backend is Running");
 });
 
-// MongoDB Connection
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
